00166333d75bd7007255a46767214186c3b03c17
Hayden Kerr <kerrster223@gmail.com>
2023-03-06 16:14:57 +0000

    added css and made mobile first

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { useState } from "react";
 function App() {
   const [location, setLocation] = useState({});
   const [searchQuery, setSearchQuery] = useState("");
-
   async function getLocation() {
     const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${searchQuery}&format=json`;
 
@@ -18,18 +17,20 @@ function App() {
   }
   return (
     <div className="App">
+      <div className="input">
+        <input
+          name="searchQuery"
+          onChange={handleSearch}
+          placeholder="Search for a City"
+        ></input>
+        <button onClick={getLocation}>explore</button>
+      </div>
       {location.display_name && (
         <p>
           {location.display_name} is at lat and lon: {location.lat} /
           {location.lon}
         </p>
       )}
-      <input
-        name="searchQuery"
-        onChange={handleSearch}
-        placeholder="Search for a City"
-      ></input>
-      <button onClick={getLocation}>explore</button>
     </div>
   );
 }
